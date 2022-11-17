db537e835d91bc97280112d00ef3f4b1ac6a1daa
pamela <pamela.oliveira219@etec.sp.gov.br>
2022-11-16 22:01:49 -0300

    incompleta

---
+++ Downloads/AtvFirebase/lib/adicionar.dart
@@ -0,0 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_firebase_teste/main.dart';
+
+class Adicionar extends StatelessWidget {
+  TextEditingController Nome = TextEditingController();
+  TextEditingController Telefone = TextEditingController();
+  TextEditingController CPF = TextEditingController();
+  TextEditingController Email = TextEditingController();
+  TextEditingController Endereco = TextEditingController();
+  TextEditingController Senha = TextEditingController();
+
+  CollectionReference ref = FirebaseFirestore.instance.collection("aluno");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              ref.add({
+                'Nome': Nome.text,
+                'CPF': CPF.text,
+                'Email': Email.text,
+                'Telefone': Telefone.text,
+                'Endereço': Endereco.text,
+                'Senha': Senha.text,
+              }).whenComplete(() => 
+              Navigator.pushReplacement(context, 
+              MaterialPageRoute(builder: (_)=> Home())));
+            },
+            child: Text('Salvar'),
+          ),
+        ],
+      ),
+      
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: Nome,
+                decoration: InputDecoration(hintText: 'Nome'),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: CPF,
+                decoration: InputDecoration(hintText: 'CPF'),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: Telefone,
+                decoration: InputDecoration(hintText: 'Telefone'),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: Endereco,
+                decoration: InputDecoration(hintText: 'Endereço'),
+              ),
+            ),
+            Expanded(
+                child: Container(
+              child: TextField(
+                controller: Email,
+                decoration: InputDecoration(hintText: 'Email'),
+              ),
+            )), Expanded(
+                child: Container(
+              child: TextField(
+                controller: Senha,
+                decoration: InputDecoration(hintText: 'Senha'),
+              ),
+            )),
+          ],
+        ),)    
+      );
+   }
+  }
---
+++ Downloads/pp/AtvFirebase/lib/alterarexcluir.dart
@@ -0,0 +1,118 @@
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_firebase_teste/main.dart';
+
+
+class AlterarExcluir extends StatefulWidget {
+  DocumentSnapshot docid;
+  AlterarExcluir({required this.docid});
+
+  @override
+  State<AlterarExcluir> createState() => _AlterarExcluirState();
+}
+
+class _AlterarExcluirState extends State<AlterarExcluir> {
+  TextEditingController Nome = TextEditingController();
+  TextEditingController Telefone = TextEditingController();
+  TextEditingController CPF = TextEditingController();
+  TextEditingController Email = TextEditingController();
+  TextEditingController Endereco = TextEditingController();
+
+  @override
+  void initState() {
+    Nome = TextEditingController(text: widget.docid.get('Nome'));
+    Telefone = TextEditingController(text: widget.docid.get('Telefone'));
+    CPF = TextEditingController(text: widget.docid.get('CPF'));
+    Email = TextEditingController(text: widget.docid.get('Email'));
+    Endereco = TextEditingController(text: widget.docid.get('Endereço'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              widget.docid.reference.update({
+                'Nome': Nome.text,
+                'Telefone': Telefone.text,
+                'CPF': CPF.text,
+                'Email': Email.text,
+                'Endereço':Endereco.text,
+              }).whenComplete(() => Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (_) => Home())));
+            },
+            child: Text('Atualizar'),
+          ),
+                    MaterialButton(
+            onPressed: () {
+              widget.docid.reference.delete().whenComplete(() => 
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (_) => Home())));
+            },
+            child: Text('Apagar'),
+          ),
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: Nome,
+                decoration: InputDecoration(hintText: 'Nome'),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: CPF,
+                decoration: InputDecoration(hintText: 'CPF'),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: Email,
+                decoration: InputDecoration(hintText: 'Email'),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+                child: TextField(
+                controller: Endereco,
+                decoration: InputDecoration(hintText: 'Endereço'),
+              ),
+            ),
+            Expanded(
+                child: Container(
+                child: TextField(
+                controller: Telefone,
+                expands: true,
+                maxLines: null,
+                decoration: InputDecoration(hintText: 'Telefone'),
+              ),
+            )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+void validadeEmail(String val) {
+  if(val.isEmpty){
+    setState(() {
+      Text("Email can not be empty");
+    });
+  }
+}
+}
+
